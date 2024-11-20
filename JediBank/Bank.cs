66c46110a9be0d4d20da488b30dc99cff1e39ea2
FlorentHajdari <florent.hajdari@hotmail.se>
2024-11-20 14:29:19 +0100

    Bank Methods

--- JediBank/Bank.cs
+++ JediBank/Bank.cs
@@ -1,12 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JediBank
+﻿namespace JediBank
 {
-    internal class Bank
+    public class Bank
     {
+        public List<User> Users { get; set; }
+
+        public Bank()
+        {
+            Users = new List<User>();
+        }
+
+        public void RunProgram()
+        {
+
+        }
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+            Console.WriteLine($"Användare {user.Username} har lagts till");
+        }
+
+    public void CreateUser()
+    {
+            Console.WriteLine("Skapa ny användare:");
+            Console.Write("Ange användarnam:");
+            string username = Console.ReadLine();
+            Console.Write("Ange lösenord: ");
+            string password = Console.ReadLine();
+
+            User newUser = new User
+            {
+                Username = username,
+                Password = password
+            };
+            AddUser(newUser);
+
+        }
+    public void LoadUsers()
+    {
+
+    }
+
+    public void ArchiveUsers()
+    {
+            Console.WriteLine("Arkiverar användare. ");
+            foreach (User user in Users)
+            {
+                Console.WriteLine($"Användarinfo: {user.Username}, {user.Password}");
+            }
+        }
+
+        public bool LoanRequirement(User user)
+        {
+            Console.WriteLine("Checkar om användare uppfyller krav: ");
+        decimal totalBalance = 0;
+
+            foreach (var account in user.Accounts)
+        {
+            totalBalance += account.Balance;
+        }
+    decimal maxLoanAmount = totalBalance * 5;
+        return user.LoanAmount <= maxLoanAmount;
     }
 }
