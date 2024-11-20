f5c81a485da34d7d8f17f80676d1c956134b77c8
glennka1337 <robloxfan03@gmail.com>
2024-11-20 11:45:29 +0100

    Added a base for a few methods.

--- JediBank/User.cs
+++ JediBank/User.cs
@@ -10,31 +10,53 @@ namespace JediBank
     {
         public string Name { get; set; }
         private string _password;
+        public bool isAdmin { get; set; }
         public List<Account> Accounts { get; set; }
 
         public void ShowAccounts()
         {
+            foreach (var account in Accounts)
+            {
+                Console.WriteLine($"{account.Name} - {account.Balance:C}");
+            }
 
         }
 
-        public bool TransferFunds(decimal amount)
+        public bool TransferFunds(decimal amount, Account account1, Account account2)
+        {
+            if (account1.Balance >= amount)
+            {
+                account1.Subtract(amount);
+                account2.Add(amount);
+                return true;
+            }
+            else return false;
+        }
+
+        public bool Withdraw(decimal amount, Account account)
+        {
+            if (account.Balance >= amount)
+            {
+                account.Subtract(amount);
+                return true;
+            }
+            else return false;
+        }
+
+/*        public bool AddFunds(decimal amount)
         {
             return true;
-        }
+        }*/
 
-        public bool Withdraw(decimal amount)
+        public bool Exchange(decimal amount, string currency, Account account1, Account account2)
         {
-            return true;
-        }
-
-        public bool AddFunds(decimal amount)
-        {
-            return true;
-        }
-
-        public void Exchange(decimal amount, string currency)
-        {
-
+            if (account2.Currency == currency && account1.Balance >= amount)
+            {
+                account1.Subtract(amount);
+                account2.Add(amount * rate);
+                return true;
+            }
+            else return false;
         }
 
         public void AddAccount()
