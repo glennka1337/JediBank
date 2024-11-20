232d5f4d55d0cb8faf90bd26d7592655c4022c39
glennka1337 <viktor.h@outlook.com>
2024-11-20 10:01:26 +0100

    Added skeleton for User class.

---
+++ JediBank/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediBank
+{
+    internal class User
+    {
+        public string Name { get; set; }
+        private string _password;
+        public List<Account> Accounts { get; set; }
+
+        public void ShowAccounts()
+        {
+
+        }
+
+        public bool TransferFunds(decimal amount)
+        {
+            return true;
+        }
+
+        public bool Withdraw(decimal amount)
+        {
+            return true;
+        }
+
+        public bool AddFunds(decimal amount)
+        {
+            return true;
+        }
+
+        public void Exchange(decimal amount, string currency)
+        {
+
+        }
+
+        public void AddAccount()
+        {
+
+        }
+    }
+}
