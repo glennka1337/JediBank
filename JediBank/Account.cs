6545d2307b036bc524d5be02af59b2a29be4da54
Serhan Gyuler <serhan.gyuler@hotmail.com>
2024-11-20 10:46:02 +0100

    Account Class And Methods For The Class

---
+++ JediBank/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediBank
+{
+    internal class Account
+    {
+        public static void Show()
+        {
+
+        }
+
+        public static void ShowHistory()
+        {
+
+        }
+
+        // Måste returna något, de därför de visar fel atm
+        public static bool Add()
+        {
+            
+        }
+
+        // Samma här
+        public static bool Subtract()
+        {
+
+        }
+
+
+    }
+}
