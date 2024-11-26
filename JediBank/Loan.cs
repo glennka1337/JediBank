3069f27b4ad0861e8247fb76988f03974e2284af
glennka1337 <viktor.h@outlook.com>
2024-11-26 15:31:56 +0100

    Created properties for the Loan class, need to clear things up before progressing further.

---
+++ JediBank/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace JediBank
+{
+    internal class Loan
+    {
+        public string LoanId { get; set; }
+        //public Currency Currency { get; set; }
+        public decimal Interest { get; set; }
+        public decimal Total { get; set; }
+        public decimal AmountPaid { get; set; }
+
+        //Will create methods after structure is agreed upon. CreateLoan(), PayOff()?
+    }
+}
